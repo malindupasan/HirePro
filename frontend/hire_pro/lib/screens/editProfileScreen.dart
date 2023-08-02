f118f3d764469feab908dd3b10b4559f8a503abe
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-08-02 10:38:19 +0530

    edit proifle ui changed and photo upload added

--- frontend/hire_pro/lib/screens/editProfileScreen.dart
+++ frontend/hire_pro/lib/screens/editProfileScreen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hire_pro/constants.dart';
-import 'package:hire_pro/widgets/MyNavigationWidget.dart';
-import 'package:hire_pro/widgets/FormInput.dart';
+import 'package:hire_pro/services/imageUpload.dart';
 import 'package:hire_pro/widgets/MainButton.dart';
-import 'package:hire_pro/widgets/MainCard.dart';
-import 'package:hire_pro/widgets/NavButton.dart';
 import 'package:hire_pro/widgets/smallButton.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
@@ -15,8 +17,25 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  String defaultImage = 'images/profile_pic.png';
+  final imageUpload = ImageUpload();
+  File? _image;
+  void changeProfilePicture(ImageSource source) async {
+    final file = await imageUpload.pickImage(source);
+    if (file != null) {
+      final croppedImage =
+          await imageUpload.crop(file: file, cropStyle: CropStyle.circle);
+      if (croppedImage != null) {
+        setState(() {
+          _image = File(
+            croppedImage.path,
+          );
+        });
+      }
+    }
+  }
+
   @override
-  TextEditingController _emailController = TextEditingController();
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
@@ -28,56 +47,95 @@ class _EditProfileState extends State<EditProfile> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text(
-                      'Edit Profile',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w500, fontSize: 18),
-                    ),
-                    Container(
-                      width: 150, // Set the desired width of the container
-                      height: 150, // Set the desired height of the container
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle, // Create a circular shape
-                        border: Border.all(
-                          color: kMainYellow, // Set the border color
-                          width: 2, // Set the border width
-                        ),
-                      ),
-                      child: Hero(
-                        tag: 'image',
-                        child: ClipOval(
-                          child: Image.asset(
-                            'images/profile_pic.png', // Replace with your image URL
-                            fit: BoxFit
-                                .cover, // Adjust the image's fit within the circular border
+                    Stack(
+                      children: [
+                        Container(
+                          width: 150, // Set the desired width of the container
+                          height:
+                              150, // Set the desired height of the container
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle, // Create a circular shape
+                            border: Border.all(
+                              color: kMainYellow, // Set the border color
+                              width: 2, // Set the border width
+                            ),
+                          ),
+
+                          child: Hero(
+                            tag: 'image',
+                            child: Center(
+                              child: FittedBox(
+                                fit: BoxFit.contain,
+                                child: CircleAvatar(
+                                  radius: 72,
+                                  backgroundColor: kMainGrey,
+                                  foregroundImage: _image != null
+                                      ? FileImage(_image!)
+                                      : null,
+                                  child: Text(
+                                    'HS',
+                                    style: TextStyle(
+                                        fontSize: 48, color: kMainYellow),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Positioned(
+                          top: 100,
+                          left: 105,
+                          child: FloatingActionButton.small(
+                              backgroundColor: kMainYellow,
+                              child: Icon(
+                                FontAwesomeIcons.camera,
+                                size: 15,
+                              ),
+                              onPressed: () => showDialog<String>(
+                                    context: context,
+                                    builder: (BuildContext context) =>
+                                        AlertDialog(
+                                      title: const Text(
+                                        'Change profile picture using,',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      content: const Text(''),
+                                      actions: <Widget>[
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            SmallButton('Camera', () async {
+                                              changeProfilePicture(
+                                                  ImageSource.camera);
+                                            }, kMainYellow, Colors.white),
+                                            SmallButton('Gallery', () async {
+                                              changeProfilePicture(
+                                                  ImageSource.gallery);
+                                            }, kMainYellow, Colors.white)
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  )),
+                        ),
+                      ],
                     ),
                     Text(
-                      "Harini Samaliarachchi",
-                      style: TextStyle(
-                        fontSize: 30,
-                        overflow: TextOverflow.ellipsis,
-                      ),
+                      'Edit Account',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
                     ),
-                    MainCard(
-                        240,
-                        335,
-                        kMainGrey,
-                        Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              EditField(
-                                  'Display Name', 'Harini Samaliarachchi'),
-                              EditField('Phone Number', '+94 769140020'),
-                              EditField(
-                                  'Email Address', 'samaliarachchih@gmail.com'),
-                            ],
-                          ),
-                        )),
+                    Column(
+                      children: [
+                        EditField('Full Name', 'Harini Samaliarachchi'),
+                        EditField('Email', 'samaliarachchih@gmail.com'),
+                        EditField('Mobile Number', '0761232323'),
+                        EditField('Password', ''),
+                      ],
+                    ),
                     MainButton("Save", () {}),
                   ],
                 ),
@@ -87,32 +145,36 @@ class _EditProfileState extends State<EditProfile> {
 }
 
 class EditField extends StatelessWidget {
-  final String label;
-  final String value;
+  String label;
+  String value;
   EditField(this.label, this.value);
 
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Text(label,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
           style: TextStyle(
-              color: kMainYellow,
-              fontSize: 12,
-              overflow: TextOverflow.ellipsis)),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-              flex: 3,
-              child: Text(value,
-                  style: TextStyle(
-                      fontSize: 15, overflow: TextOverflow.ellipsis))),
-          SizedBox(
-            width: 10,
+            color: Colors.grey,
+            fontSize: 12,
           ),
-          Expanded(child: SmallButton('Edit', () {}, kMainYellow, Colors.white))
-        ],
-      )
-    ]);
+        ),
+        SizedBox(height: 6),
+        TextFormField(
+          initialValue: value,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        SizedBox(height: 6),
+      ],
+    );
   }
 }
