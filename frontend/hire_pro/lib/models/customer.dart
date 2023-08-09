39048755e128e3868742149c53273ad50bb747a2
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-08-10 00:30:46 +0530

    login

--- frontend/hire_pro/lib/models/customer.dart
+++ frontend/hire_pro/lib/models/customer.dart
@@ -1,7 +1,22 @@
 class Customer {
-  final String? name;
-  final String? email;
-  final String? contact;
-  final int? loyaltyPoints;
-  Customer(this.name, this.email, this.contact, this.loyaltyPoints);
+  final String name;
+  final String email;
+  final String contact;
+  final int loyalityPoints;
+
+  const Customer({
+    required this.name,
+    required this.email,
+    required this.contact,
+    required this.loyalityPoints,
+  });
+
+  factory Customer.fromJson(Map<String, dynamic> json) {
+    return Customer(
+      name: json['name'],
+      email: json['email'],
+      contact: json['contact'],
+      loyalityPoints: json['loyalty_points'],
+    );
+  }
 }
--- frontend/hire_pro/lib/services/api.dart
+++ frontend/hire_pro/lib/services/api.dart
@@ -1,33 +1,23 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:hire_pro/models/customer.dart';
 import 'package:hire_pro/services/routes.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  late Customer customer;
-  static const baseUrl = 'http://192.168.164.1:3000/';
-  getUser() async {
-    var url = Uri.parse('${baseUrl}getdata');
-    var reqBody = {"id": "1"};
-    try {
-      final res = await http.get(
-        url,
-        headers: {'Content-Type': 'application/json','authorization':'$sesstionToken'},
-       
-      );
+  Future<Customer> getData() async {
+    final response = await http.get(
+      Uri.parse('$url.getdata'),
+      headers: {
+        'Content-Type': 'application/json',
+         'Authorization': 'Bearer $sesstionToken',
+      },
+    );
 
-      if (res.statusCode == 200) {
-        var data = jsonDecode(res.body);
-        data['name'] = customer.name;
-        data['email'] = customer.email;
-        data['contact'] = customer.contact;
-        data['loyalty_points'] = customer.loyaltyPoints;
-        print('success');
-        return customer;
-      } else {}
-    } catch (e) {
-      print(e.toString());
+    if (response.statusCode == 200) {
+      return Customer.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load customer');
     }
   }
 }
