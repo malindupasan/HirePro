47e20d9275134b531da4a03d3f276079576d6287
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-09-24 10:45:02 +0530

    errors fixed

--- frontend/hirepro/lib/widgets/TaskCardOngoing.dart
+++ frontend/hirepro/lib/widgets/TaskCardOngoing.dart
@@ -10,7 +10,8 @@ class TaskCardOngoing extends StatefulWidget {
   late double price;
   late String image;
   late double progress;
-  TaskCardOngoing(this.serviceProvider, this.jobType, this.price, this.image,this.progress);
+  TaskCardOngoing(this.serviceProvider, this.jobType, this.price, this.image,
+      this.progress);
 
   @override
   State<TaskCardOngoing> createState() => _TaskCardOngoingState();
@@ -20,77 +21,13 @@ class _TaskCardOngoingState extends State<TaskCardOngoing> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(5),
-      child: Row(children: [
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.jobType,
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      FontAwesomeIcons.buildingUser,
-                      size: 15,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      widget.serviceProvider,
-                      style: TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  'Service provider',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                Text(
-                  'Rs.' + widget.price.toString().split('.')[0],
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: kMainYellow,
-                      fontWeight: FontWeight.bold),
-                ),
-                new LinearPercentIndicator(
-                  barRadius: Radius.circular(10),
-                  width: 140.0,
-                  lineHeight: 14.0,
-                  percent: widget.progress,
-                  backgroundColor: Colors.grey[300],
-                  progressColor: Colors.amberAccent,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'View More',
-                    ),
-                    SizedBox(width: 10),
-                    Icon(FontAwesomeIcons.arrowRight, size: 12)
-                  ],
-                )
-              ]),
-        )),
-        Expanded(child: Image.asset(widget.image))
-      ]),
+      padding: const EdgeInsets.all(5),
       height: 150,
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           color: kMainGrey,
-          boxShadow: [
+          boxShadow: const [
             BoxShadow(
               color: Colors.grey,
               blurRadius: 3.0,
@@ -98,6 +35,72 @@ class _TaskCardOngoingState extends State<TaskCardOngoing> {
               offset: Offset(2.0, 2.0),
             ),
           ]),
+      child: Row(children: [
+        Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.jobType,
+                      style: const TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          FontAwesomeIcons.buildingUser,
+                          size: 15,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          widget.serviceProvider,
+                          style: const TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      'Service provider',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Text(
+                      'Rs.' + widget.price.toString().split('.')[0],
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: kMainYellow,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    LinearPercentIndicator(
+                      barRadius: Radius.circular(10),
+                      width: 140.0,
+                      lineHeight: 14.0,
+                      percent: widget.progress,
+                      backgroundColor: Colors.grey[300],
+                      progressColor: Colors.amberAccent,
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'View More',
+                        ),
+                        SizedBox(width: 10),
+                        Icon(FontAwesomeIcons.arrowRight, size: 12)
+                      ],
+                    )
+                  ]),
+            )),
+        Expanded(flex: 2, child: Image.asset(widget.image))
+      ]),
     );
   }
 }
